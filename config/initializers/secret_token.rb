7e50db2cb2ed06b826c6a5a69b597c6710d5bf6a
OneDivZero <onedivzero@gmx.de>
2012-04-16 19:08:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '9e318515d14ef810435dbf0d478b7073b768900763d72dda951925363af991dcc56343dcf241cd1600864d9f2a6090eee6733ba0df295c5bc060d677c068021b'
